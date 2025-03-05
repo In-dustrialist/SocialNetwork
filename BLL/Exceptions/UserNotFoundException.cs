d40198fc93adbd71940c02d8497698f4c567781a
Eugene P. <pinaevmsk@gmail.com>
2025-03-05 04:24:25 +0400

    Add project files.

---
+++ BLL/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace SocialNetwork.BLL.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+    }
+}
---
+++ PLL/Views/UserAddFriendView.cs
@@ -0,0 +1,37 @@
+﻿
+using SocialNetwork.BLL.Services;
+using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.PLL.Helpers;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class UserAddFriendView
+    {
+        UserService userService;
+
+        public UserAddFriendView(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public void Show(int userId)
+        {
+            Console.Write("Введите почтовый адрес друга: ");
+            string friendEmail = Console.ReadLine();
+
+            try
+            {
+                userService.AddFriend(userId, friendEmail);
+                SuccessMessage.Show("Пользователь успешно добавлен в друзья!");
+            }
+            catch (UserNotFoundException)
+            {
+                AlertMessage.Show("Пользователь с таким email не найден.");
+            }
+            catch (Exception)
+            {
+                AlertMessage.Show("Произошла ошибка при добавлении в друзья.");
+            }
+        }
+    }
+}
